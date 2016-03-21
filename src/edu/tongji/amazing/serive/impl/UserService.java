6edbb7a99e099096b8300d58a2737b4d4fe1f0ff
Happyfine <772773671@qq.com>
2016-03-21 23:44:15 +0800

    完善框架，利用注解完成用户注册功能

---
+++ src/edu/tongji/amazing/serive/impl/UserService.java
@@ -0,0 +1,54 @@
+package edu.tongji.amazing.serive.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import edu.tongji.amazing.dao.IUserDao;
+import edu.tongji.amazing.dao.impl.UserDao;
+import edu.tongji.amazing.model.User;
+import edu.tongji.amazing.service.IUserService;
+
+@Service
+public class UserService implements IUserService{
+
+	@Resource
+	private IUserDao userdao;
+
+	@Override
+	public User checkUser(User user) {
+		// TODO Auto-generated method stub
+		return userdao.checkUser(user);
+	}
+
+	@Override
+	public boolean addUser(User user) {
+		// TODO Auto-generated method stub
+		return userdao.addUser(user);
+	}
+
+	@Override
+	public boolean updateUser(User user) {
+		// TODO Auto-generated method stub
+		return userdao.updateUser(user);
+	}
+
+	@Override
+	public boolean RvokeAccount(long identity) {
+		// TODO Auto-generated method stub
+		return userdao.RvokeAccount(identity);
+	}
+
+	@Override
+	public boolean ResumeAccount(long identity) {
+		// TODO Auto-generated method stub
+		return userdao.ResumeAccount(identity);
+	}
+
+	@Override
+	public User getUserbyIndentity(long identity) {
+		// TODO Auto-generated method stub
+		return userdao.getUserbyIndentity(identity);
+	}
+
+}
