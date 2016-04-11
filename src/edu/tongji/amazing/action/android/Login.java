aff2a21554689f916e41c4c136ad14ebbe7e3e93
happy <772773671@qq.com>
2016-04-11 11:02:27 +0800

    before merge

--- src/edu/tongji/amazing/action/android/Login.java
+++ src/edu/tongji/amazing/action/android/Login.java
@@ -19,16 +19,33 @@ public class Login extends ActionSupport {
 	private Map<String, Object> data;
 	@Resource
 	private CarOwnerService service;
+	
+	private String username;
+	private String password;
 	@Override
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
-		data = new HashMap<String,Object>();
-		CarOwner carowner = service.getUserbyIndentity("130823200001290001");
-		data.put("carowner", carowner);
+		System.out.println("username is "+ username);
+		System.out.println("password is "+ password);
+		//data = new HashMap<String,Object>();
+		//CarOwner carowner = service.getUserbyIndentity("130823200001290001");
+		//data.put("carowner", carowner);
 		return "success";
 	}
 	public Map<String, Object> getData() {
 		return data;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 
