d0c53161fb4bfcbdc35f16d1def4e938115cff27
happy <772773671@qq.com>
2016-04-11 12:03:08 +0800

    加入验证码接口

--- src/edu/tongji/amazing/tool/SendMessage.java
+++ src/edu/tongji/amazing/tool/SendMessage.java
@@ -1,4 +1,4 @@
-package edu.tongji.amazingad.tools;
+package edu.tongji.amazing.tool;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
@@ -10,6 +10,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -17,13 +18,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * 短信http接口的java代码调用示例
  * 基于Apache HttpClient 4.3
  *
  */
-
+@Service("sendmessage")
 public class SendMessage {
 
     //智能匹配模版发送接口的http地址
@@ -34,15 +36,30 @@ public class SendMessage {
     //修改为您的apikey.apikey可在官网（http://www.yuanpian.com)登录后获取
     static String apikey = "1d2113d4b2e5178c1d097916c5f691e8";
     
-    @Test
-    public void test() throws IOException, URISyntaxException{
+    private String phonenumber;
+    private int code;
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+    
+
+	public int getCode() {
+		return code;
+	}
+
+
+//	@Test
+    public void Send() throws IOException, URISyntaxException{
 
 
         //修改为您要发送的手机号
-        String mobile = "15021799675";
+        String mobile = phonenumber;
         /**************** 使用智能匹配模版接口发短信(推荐) *****************/
         //设置您要发送的内容(内容必须和某个模板匹配。以下例子匹配的是系统提供的1号模板）
-        String text = "【WOW】您的验证码是123321";
+        Random random = new Random();
+        code = (int)(random.nextFloat()*1000000);
+//        System.out.println("code is "+code);
+        String text = "【WOW】您的验证码是"+code;
         //发短信调用示例
         sendSms(apikey, text, mobile);
     }
