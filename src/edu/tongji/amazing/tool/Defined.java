c0449db0be7f4741ad60ae004d349118625a7146
happy <772773671@qq.com>
2016-05-31 16:55:51 +0800

    merge admin

--- src/edu/tongji/amazing/tool/Defined.java
+++ src/edu/tongji/amazing/tool/Defined.java
@@ -7,7 +7,7 @@ public class Defined {
 
 	public final String Error = "errCode";
 	public final String apikey = "1d2113d4b2e5178c1d097916c5f691e8";
-	public final String baseurl = "http://10.60.42.70:8080";
+	public final String baseurl = "http://10.60.42.70:8888";
 	public final String basepath = "D:\\Eclipse\\apache-tomcat\\webapps";
 	public static String Baiduapk ="buPZFGnRf6EhZaEKHHdQswbMRMSqcGx5";
 	public final int SUCCESS = 1;//成功
